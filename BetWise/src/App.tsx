be0345c88f3620b5dd7d3626a58e5f7a178b0525
Austin Mthombeni <2586407@students.wits.ac.za>
2025-07-03 00:23:46 +0200

    feat(homepage): initial homepage layout complete

--- BetWise/src/App.tsx
+++ BetWise/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
-function App() {
+import './App.css'
+import type { JSX } from "react";
+
+
+function App(): JSX.Element{
 
   return (
-    <div>
-      <h1>BetWise.</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path = "/" element={<HomePage/>} />
+
+      </Routes>
+    </Router>
   )
 }
 
