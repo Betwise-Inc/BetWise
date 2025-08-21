ad5ae703e14e0531c694823958b1f73d844b248e
LEVY315 <132851230+LEVY315@users.noreply.github.com>
2025-08-21 02:27:06 +0200

    Small change

--- BetWise/src/App.tsx
+++ BetWise/src/App.tsx
@@ -1,31 +1,28 @@
-import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LangingPage";
 import HomePage from "./pages/HomePage";
 import Auth from "./pages/Auth";
 import InsightsPage from "./pages/insights";
 import BetSlip from "./pages/Betslip";
-import { UserProvider } from "./Hooks/UserContext"; 
-import './App.css'
+import { UserProvider } from "./Hooks/UserContext";
+import "./App.css";
 import type { JSX } from "react";
 
-
-function App(): JSX.Element{
-
+function App(): JSX.Element {
   return (
-    
     <UserProvider>
       <Router>
         <Routes>
-          <Route path = "/" element={<LandingPage/>} />
-          <Route path = "/insights" element={<InsightsPage/>} />
-          <Route path = "/Auth" element={<Auth/>} />
-          <Route path = "/home" element={<HomePage/>} />
-          <Route path = "/generate-betslip" element={<BetSlip/>} />
+          <Route path="/" element={<LandingPage />} />
+
+          <Route path="/Auth" element={<Auth />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/generate-betslip" element={<BetSlip />} />
+          <Route path="/insights" element={<InsightsPage />} />
         </Routes>
       </Router>
     </UserProvider>
-  )
+  );
 }
 
-export default App
-// This is a comment
+export default App;
